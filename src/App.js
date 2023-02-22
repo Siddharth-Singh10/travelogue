21707c9d6ffb80c4cf60fc98bbcbbe77267a0402
Siddharth-Singh10 <siddharthsingh10402@gmail.com>
2023-02-22 15:01:44 +0530

    Yo..

---
+++ src/App.js
@@ -0,0 +1,25 @@
+//import logo from './logo.svg';
+import "./App.css";
+import Home from "./Home";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import Trip from "./Trip";
+import Tripdetails from "./Tripdetails";
+import MapT from "./Map";
+import Media from "./Media";
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/trips" element={<Trip />} />
+        <Route path="/tripdetail" element={<Tripdetails />} />
+        <Route path="/map" element={<MapT />} />
+        <Route path="/media" element={<Media />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
